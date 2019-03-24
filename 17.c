840ade69a8e8cd075bffee1b898ca7be24ac8f3f
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-03-24 21:53:05 +0530

    Update 17.c

--- 17.c
+++ 17.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main()
 {
-    int n,k,ak[100],i,j,c=0;
-    scanf("%d%d",&n,&k);
-    for(i=0;i<n;i++)
+    int n1,k1,ak1[100],i1,j1,c1=0;
+    scanf("%d%d",&n1,&k1);
+    for(i1=0;i1<n1;i1++)
     {
-    	scanf("%d",&ak[i]);
+    	scanf("%d",&ak1[i]);
     }
-    for(i=0;i<n;i++)
+    for(i1=0;i1<n1;i1++)
     {  
-    	for(j=i+1;j<n;j++)
+    	for(j1=i1+1;j1<n1;j1++)
     	{
-    		if(ak[i]+ak[j]==k)
+    		if(ak1[i1]+ak1[j1]==k1)
     		{
-    			c++;
+    			c1++;
     		}
     	}
    	}
-    if(c>=1)
+    if(c1>=1)
     {
     	printf("yes");
     }
