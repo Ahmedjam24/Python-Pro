4d2641b251b53dd3e500a3b6b227be348673c549
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-03-24 21:39:31 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,14 @@
+
+n1 = int(input())
+in_num = list(map(int, input().split()))
+ 
+no_of_one = []
+for i in in_num:
+    count = 0
+    while i:
+        i &= (i-1)
+        count += 1
+    no_of_one.append(count)
+ 
+result = sorted(zip(no_of_one, in_num), reverse=True)
+print(*[num for _, num in result], sep='\n')
