666726d11dce72960d2e60c180795dafe3fff479
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-03-25 13:36:45 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -13,9 +13,9 @@ for i in range(0,len(d2)):
     k=p1.index(' ')
     p1[k]='0'
   m2.append(''.join(p1))
- k=0
+k=0
 for i in m2:
-  k++
+  k+=1
   print(i)
   if(k%a2==0 && k!=len(m2)):
     print("\n")
